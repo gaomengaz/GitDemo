d2547a1375230deae73219b3914d6d09c70bef1d
gaomeng <gaomeng@gaomengdeMacBook-Pro.local>
2014-04-04 14:55:36 +0800

    Initial Commit

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by gaomeng on 14-4-4.
+//  Copyright (c) 2014年 gaomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitDemo
+//
+//  Created by gaomeng on 14-4-4.
+//  Copyright (c) 2014年 gaomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
